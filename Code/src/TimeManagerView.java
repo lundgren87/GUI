09cbdc409f0ad030d3281e06093a3f21a59e2da7
kimfk <kimfeenstrakuiper@gmail.com>
2014-02-05 10:52:33 +0100

    Imroved the view
    Bot done yet, of course :) But now contains more panels

--- Code/src/TimeManagerView.java
+++ Code/src/TimeManagerView.java
@@ -1,22 +1,41 @@
 import java.awt.BorderLayout;
+import java.awt.GridLayout;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 
 public class TimeManagerView {
 	
-	private JFrame frame = new JFrame("Time Manager");
-	private JButton addButton =  new JButton("Add");
+	private JFrame mainFrame = new JFrame("Time Manager");
+	private JPanel titelPanel = new JPanel(); 	//The panel which shows the titel of the current tab
+	private JPanel mainPanel = new JPanel();	//The panel where the tasks are shown
+	private JPanel tabPanel = new JPanel();		//The panel where different sortings are shown (as tabs)
+	private JPanel addPanel = new JPanel();		//The panel where one can add a new task
+	
+	private JButton addButton =  new JButton("Add");	//The button to add a new task
+	private JButton logoutButton = new JButton("Log Out");	//The  button to log out
 	
 	TimeManagerView(){	
-
 		
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.pack();
-		frame.getContentPane().add(BorderLayout.SOUTH, addButton);
-		frame.setSize(700, 700);
-		frame.setVisible(true);
+		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		mainFrame.pack();
+		
+		//temporary try out of titel pannel
+
+		titelPanel.setLayout(new GridLayout(1, 2));
+		titelPanel.add(new JLabel("Titel"));
+		titelPanel.add(logoutButton);
+		
+		mainFrame.getContentPane().add(BorderLayout.NORTH, titelPanel);
+		mainFrame.getContentPane().add(BorderLayout.WEST, tabPanel);
+		mainFrame.getContentPane().add(BorderLayout.SOUTH, addButton);
+		mainFrame.getContentPane().add(BorderLayout.CENTER, mainPanel);
+		
+		mainFrame.setSize(700, 700);
+		mainFrame.setVisible(true);
 	}
 
 }
