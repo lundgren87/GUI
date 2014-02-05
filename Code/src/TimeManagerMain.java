e66f0ae5115bd1d9f6a79a149269378ce136c7a8
kimfk <kimfeenstrakuiper@gmail.com>
2014-02-05 13:21:06 +0100

    Test again

--- Code/src/TimeManagerMain.java
+++ Code/src/TimeManagerMain.java
@@ -1,6 +1,7 @@
 
 public class TimeManagerMain {
 
+	//This is Kim, HELOO!
 	public static void main(String[] args) {
 		TimeManagerView view = new TimeManagerView();
 		TimeManagerModel model = new TimeManagerModel();
