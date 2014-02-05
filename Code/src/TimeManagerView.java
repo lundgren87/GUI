e335c1da6050fcedf6ff210fc2f6dbf095c48cbd
kimfk <kimfeenstrakuiper@gmail.com>
2014-02-05 14:09:01 +0100

    new update

--- Code/src/TimeManagerView.java
+++ Code/src/TimeManagerView.java
@@ -6,15 +6,17 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+import javax.swing.JTextArea;
 
 
 public class TimeManagerView {
 	
 	private JFrame mainFrame = new JFrame("Time Manager");
 	private JPanel titelPanel = new JPanel(); 	//The panel which shows the titel of the current tab
-	private JPanel mainPanel = new JPanel();	//The panel where the tasks are shown
-	//private JPanel tabPanel = new JPanel();		//The panel where different sortings are shown (as tabs)
 	private JPanel addPanel = new JPanel();		//The panel where one can add a new task
+	
+	private JTabbedPane categotoryTab = new JTabbedPane(JTabbedPane.LEFT); //The tabPanel
 	
 	private JButton addButton =  new JButton("Add");	//The button to add a new task
 	private JButton logoutButton = new JButton("Log Out");	//The  button to log out
@@ -30,18 +32,28 @@ public class TimeManagerView {
 		titelPanel.add(new JLabel("Titel"));
 		titelPanel.add(logoutButton);
 		
-		//Conent addPanel
+		//Content addPanel
 		addPanel.setBackground(Color.yellow);
 		addPanel.add(addButton);
 		
-		//Content mainPanel
-		mainPanel.setBackground(Color.blue);
+		//Content tab
+		//mainPanel.setBackground(Color.blue);
+		categotoryTab.addTab("Work", null, new JPanel(), "Show work related tasks");
+		categotoryTab.addTab("School", null, new JPanel(), "Show school related tasks");
+		categotoryTab.addTab("House", null, new JPanel(), "Show house related tasks");
+		categotoryTab.setTabLayoutPolicy(JTabbedPane.WRAP_TAB_LAYOUT);
+		
+		//Content addPanel
+		JTextArea nameActivity = new JTextArea("Name activity"); 
+		String input = nameActivity.getText();
+		//addPanel.add(new )
 		
 		mainFrame.getContentPane().add(BorderLayout.NORTH, titelPanel);
-		mainFrame.getContentPane().add(BorderLayout.CENTER, mainPanel);
+		mainFrame.getContentPane().add(BorderLayout.CENTER, categotoryTab);
 		mainFrame.getContentPane().add(BorderLayout.SOUTH, addPanel);
 		
-		mainFrame.setSize(700, 700);
+		mainFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		//mainFrame.setSize( JFrame.MAXIMIZED_VERT, JFrame.MAXIMIZED_HORIZ);
 		mainFrame.setVisible(true);
 	}
 
