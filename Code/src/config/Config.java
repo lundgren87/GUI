6643552490a5274ff9770d4585eb94ba12c877f0
pbjrk <pontus.bjoerk@gmail.com>
2014-02-19 11:46:34 +0100

    Config and Language handling implemented

--- Code/src/config/Config.java
+++ Code/src/config/Config.java
@@ -1,7 +1,139 @@
 package config;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Properties;
+
+/**
+ * Configuration file handler.
+ * 
+ * @author Pontus, Sven
+ *
+ */
 public class Config {
-	public static String DBFile = "assets/DBFile";
-	public static String Images = "/images/";
-	public static String[] availableLanguages = {"English", "Swedish"};
+	public static String DBFile;
+	public static String Images;
+	public static String startupLanguage;
+	
+	/**
+	 * Loads general properties from config file.
+	 */
+	public static void loadConfig() {
+		Properties prop = new Properties();
+		InputStream input = null;
+		try {
+			input = new FileInputStream("config.properties");
+			
+			DBFile 			= prop.getProperty("DBFile",          "assets/DBFile" );
+			Images 			= prop.getProperty("Images",          "/images/"      );
+			startupLanguage = prop.getProperty("startupLanguage", "English"       );
+			
+		}
+		catch (IOException e){
+			e.printStackTrace();
+		}
+		finally {
+			if (input != null) {
+				try {
+					input.close();
+				}
+				catch (IOException e2){
+					e2.printStackTrace();
+				}
+			} 
+		}
+	}
+	
+
+	/**
+	 * Saves general properties to config file.
+	 */
+	public static void saveConfig() {
+		Properties prop = new Properties();
+		OutputStream output = null;
+		try {
+			output = new FileOutputStream("config.properties");
+			
+			prop.setProperty("DBFile",DBFile);
+			prop.setProperty("startupLanguage",startupLanguage);
+			
+			prop.store(output, null);
+		}
+		catch (IOException e){
+			e.printStackTrace();
+		}
+		finally {
+			if (output != null) {
+				try {
+					output.close();
+				}
+				catch (IOException e2){
+					e2.printStackTrace();
+				}
+			} 
+		}
+	}
+	
+	/**
+	 * Loads a the value of the property key from config file.
+	 * @param key
+	 * @param defaultValue
+	 * @return Value of key from config file
+	 */
+	public static String loadProperty(String key,String defaultValue) {
+		Properties prop = new Properties();
+		InputStream input = null;
+		try {
+			input = new FileInputStream("config.properties");
+			
+			return prop.getProperty(key, defaultValue);
+		}
+		catch (IOException e){
+			e.printStackTrace();
+			return "Error";
+		}
+		finally {
+			if (input != null) {
+				try {
+					input.close();
+				}
+				catch (IOException e2){
+					e2.printStackTrace();
+				}
+			} 
+		}
+	}
+	
+	/**
+	 * Saves a property value to config file.
+	 * @param key
+	 * @param value
+	 */
+	public static void saveProperty(String key,String value) {
+		Properties prop = new Properties();
+		OutputStream output = null;
+		try {
+			output = new FileOutputStream("config.properties");
+			
+			prop.setProperty(key, value);
+			
+			prop.store(output, null);
+		}
+		catch (IOException e){
+			e.printStackTrace();
+		}
+		finally {
+			if (output != null) {
+				try {
+					output.close();
+				}
+				catch (IOException e2){
+					e2.printStackTrace();
+				}
+			} 
+		}
+	}
 }
--- Code/src/config/LanguageRepository.java
+++ Code/src/config/LanguageRepository.java
@@ -1,29 +1,43 @@
 package config;
 
-import java.util.List;
+import java.util.HashMap;
 
 public class LanguageRepository {
-	static int currentLanguage = 0;
-	
-	static void setCurrentLanguage(int newLanguage) {
+	static String currentLanguage = Config.startupLanguage;
+	public static String[] availableLanguages = {"English", "Swedish"};
+	static HashMap<String,String> languageMap = new HashMap<String,String>();
+	static Boolean isInitialized = false;
+
+	static void setCurrentLanguage(String newLanguage) {
 		currentLanguage = newLanguage;
+		init();
 	}
 	
-	static int getCurrentLanguage() {
+	static String getCurrentLanguage() {
 		return currentLanguage;
 	}
 	
-	static String getString(String stringName) {
-		String languageName = Config.availableLanguages[currentLanguage];
-		Language<?> act;
-		try {
-			act = Class.forName("config."+languageName);
-		} catch (ClassNotFoundException e) {
-			System.out.println("Language" + languageName + "does not exist.");
-			e.printStackTrace();
+	static void init(){
+		isInitialized = true;
+		switch (currentLanguage) {
+			
+			// Insert text mappings for English here
+			case "English":
+				languageMap.put("EXAMPLE_LABLE","Example_text");
+				break;
+			
+			// Insert text mappings for Swedish here
+			case "Swedish":
+				languageMap.put("EXAMPLE_LABLE","Exempel_text");
+				break;
+				
+			default:
+				System.out.println("Invalid language");
 		}
-		return act.getString(stringName);
-		
+	}
+	static String getString(String stringName) {
+		if (!isInitialized) init();
+		return languageMap.get(stringName);
 	}
 }
 
