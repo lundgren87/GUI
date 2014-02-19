ee281926856d543d1b05fd673af14e8bb068ea5c
lundgren87 <lundgren87@gmail.com>
2014-02-19 12:12:47 +0100

    Documented LanguageRepository

--- Code/src/config/LanguageRepository.java
+++ Code/src/config/LanguageRepository.java
@@ -2,39 +2,73 @@ package config;
 
 import java.util.HashMap;
 
+
+/**
+ * @author Sven, Pontus
+ *
+ * Language repository class
+ */
 public class LanguageRepository {
 	static String currentLanguage = Config.startupLanguage;
 	public static String[] availableLanguages = {"English", "Swedish"};
 	static HashMap<String,String> languageMap = new HashMap<String,String>();
 	static Boolean isInitialized = false;
 
+	
+	/**
+	 * @param newLanguage
+	 * If newLanguage is different from currentLanguage,
+	 * sets newLanguage to currentLanguage and runs init
+	 */
 	static void setCurrentLanguage(String newLanguage) {
-		currentLanguage = newLanguage;
-		init();
+		if(currentLanguage != newLanguage) {
+			currentLanguage = newLanguage;
+			init();
+		}
 	}
 	
+	
+	/**
+	 * @return currentLanguage
+	 * Gets the current language
+	 */
 	static String getCurrentLanguage() {
 		return currentLanguage;
 	}
 	
+	
+	/**
+	 * Initializes languageMap to contain the data matching the current language
+	 * sets isInitialized to true if successful
+	 */
 	static void init(){
-		isInitialized = true;
 		switch (currentLanguage) {
 			
 			// Insert text mappings for English here
 			case "English":
 				languageMap.put("EXAMPLE_LABLE","Example_text");
+				isInitialized = true;
 				break;
 			
 			// Insert text mappings for Swedish here
 			case "Swedish":
 				languageMap.put("EXAMPLE_LABLE","Exempel_text");
+				isInitialized = true;
 				break;
 				
+			// If currentLanguage is invalid
 			default:
 				System.out.println("Invalid language");
 		}
 	}
+	
+	
+	/**
+	 * @param stringName
+	 * @return value matching key stringName
+	 * Returns the value matching the key stringName in languageMap
+	 * Initializes languageMap if it is not initialized already
+	 */
 	static String getString(String stringName) {
 		if (!isInitialized) init();
 		return languageMap.get(stringName);
